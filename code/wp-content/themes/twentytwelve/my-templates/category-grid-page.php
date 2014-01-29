a4ac3fae31118fa20d315ec9ddc68b7e84a0c198
Michael Millar <mmillar@verto.ca>
2014-01-28 21:57:13 -0500

    trying something with grid page

--- code/wp-content/themes/twentytwelve/my-templates/category-grid-page.php
+++ code/wp-content/themes/twentytwelve/my-templates/category-grid-page.php
@@ -5,30 +5,30 @@ Template Name: SE Category Grid Page
 
 get_header();
 
-query_posts( array ( 'category_name' => get_post_meta(get_the_ID(), "category", true), 'posts_per_page' => 20 ) );
 $layout = get_post_meta(get_the_ID(), "grid-layout", true);
 $submenu = get_post_meta(get_the_ID(), "sub-menu", true);
 ?>
 
-	<?php while ( have_posts() ) : the_post(); ?>
-
 	<?php if($submenu) { ?>
 
 	<div id="page-navigation" class="sub-navigation">
-		<?php wp_nav_menu( array( 'theme_location' => get_post_meta(get_the_ID(), "sub-menu", true), 'menu_class' => 'nav-menu' ) ); ?>
+		<?php wp_nav_menu( array( 'theme_location' => $submenu, 'menu_class' => 'nav-menu' ) ); ?>
 	</div>
 
-	<?php } ?>
+	<?php } 
+
+	query_posts( array ( 'category_name' => get_post_meta(get_the_ID(), "category", true), 'posts_per_page' => 20 ) );
+	?>
 
 	<div id="primary" class="site-content" style="width: 100%; max-width: 960px">
 		<div id="content" role="main">
 
+		<?php while ( have_posts() ) : the_post(); ?>
 			<?php get_template_part( 'content', $layout ); ?>
 			<?php comments_template( '', true ); ?>
-			
+		<?php endwhile; // end of the loop. ?>
+
 		</div><!-- #content -->
 	</div><!-- #primary -->
-
-	<?php endwhile; // end of the loop. ?>
 
 <?php get_footer(); ?>
