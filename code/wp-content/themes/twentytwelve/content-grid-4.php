a3c5effea8afbfbcbd87700e40a72ba0b52fdf28
Michael Millar <mmillar@verto.ca>
2014-01-29 21:53:28 -0500

    updated style.css to support galleries

--- code/wp-content/themes/twentytwelve/content-grid-4.php
+++ code/wp-content/themes/twentytwelve/content-grid-4.php
@@ -10,7 +10,7 @@
  */
 ?>
 
-	<article id="post-<?php the_ID(); ?>" class="grid-4">
+	<article id="post-<?php the_ID(); ?>" class="grid-4" onclick="window.location = '<?php the_permalink(); ?>';" style="cursor:pointer">
 		<?php the_post_thumbnail(array(300,300)); ?>
 		<span class="grid-title">
 			<a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a>
